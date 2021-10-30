c84d52ec99da37377de85db24b15a384cf2339a6
Luis Abasto <labasto@cognosys.com.bo>
2021-10-29 23:32:19 -0400

    se esta empezando el tutorial

--- Codigo/Abasto.Library1/BulkExtensions/BulkConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Abasto.Library.BulkExtensions
-{
-    public class BulkConfig
-    {
-        public string TableName { get; set; }
-        public bool ColumnImput { get; set; }
-    }
-}
