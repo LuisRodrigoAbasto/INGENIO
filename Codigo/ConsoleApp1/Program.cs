d5dc14fbb7167fe291c69fa6ac8741e4cbaf5059
Luis Abasto <labasto@cognosys.com.bo>
2022-01-15 08:54:37 -0400

    cambios

--- Codigo/ConsoleApp1/Program.cs
+++ Codigo/ConsoleApp1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Abasto.Library.DevExtreme;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -11,10 +9,10 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var lista=new List<string>() { "ok","mam"};
-            var result = lista.AsQueryable().Paginate();           
-            foreach (var item in result.data) {
-                Console.WriteLine(item);
-            }
+            //var result = lista.AsQueryable().Paginate();
+            //foreach (var item in result.data) {
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
