1631a28b396729372503ebb120ea9992ce177008
Luis Abasto <labasto@cognosys.com.bo>
2022-02-19 04:01:07 -0400

    cambios mejorados

--- Codigo/Abasto.Library/Interfaces/IPaginate.cs
+++ Codigo/Abasto.Library/Interfaces/IPaginate.cs
@@ -9,13 +9,7 @@ namespace Abasto.Library.Interfaces
 {
     public interface IPaginate
     {
-        Task<IPaginateResult<T>> PaginateResultAsync<T>(IQueryable<T> source, string filter, Action<QueryFilter> options) where T : class;
-        IPaginateResult<T> PaginateResult<T>(IQueryable<T> source, string filter, Action<QueryFilter> options) where T : class;
-        Task<IPaginateResult<T>> PaginateResultAsync<T>(IQueryable<T> source, Action<QueryFilter> options) where T : class;
-        IPaginateResult<T> PaginateResult<T>(IQueryable<T> source, Action<QueryFilter> options) where T : class;
-        Task<IPaginateResult<T>> PaginateResultAsync<T>(IQueryable<T> source, string filter) where T : class;
-        IPaginateResult<T> PaginateResult<T>(IQueryable<T> source, string filter) where T : class;
-        Task<IPaginateResult<T>> PaginateResultAsync<T>(IQueryable<T> source) where T : class;
-        IPaginateResult<T> PaginateResult<T>(IQueryable<T> source) where T : class;
+        Task<IPaginateResult<T>> PaginateResultAsync<T>() where T : class;
+        IPaginateResult<T> PaginateResult<T>() where T : class;
     }
 }
